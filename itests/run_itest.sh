959a3ac6905a52eb299aea55d601138904512a61
Jonathan Zernik <yzernik@gmail.com>
2019-10-18 23:41:22 -0700

    Fix itest volume persistence in docker-compose. (#29)

--- itests/run_itest.sh
+++ itests/run_itest.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd docker
-docker-compose -f docker-compose.test.yml down --volumes
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml up -d
+cd itests
+docker-compose down --volumes
+docker-compose build
+docker-compose up -d
 
 # Initialize the blockchain with miner rewards going to alice.
 sleep 10
 alice_address=$(docker exec -it lnd_alice lncli --network=simnet newaddress np2wkh | jq .address -r)
 MINING_ADDRESS=$alice_address docker-compose up -d btcd
 echo "Mining 400 blocks to address: $alice_address ..."
-docker-compose -f docker-compose.test.yml run btcctl generate 400
+docker-compose run btcctl generate 400
 echo "Finished mining blocks."
 sleep 10
 
 echo "Running test.sh...."
-docker-compose -f docker-compose.test.yml run test ./test.sh
+docker-compose run test ./test.sh
